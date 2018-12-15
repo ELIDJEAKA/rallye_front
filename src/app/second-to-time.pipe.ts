1ea3f88c2833539231a7d9d248407d1c88c05e51
elidje aka emmanuel <akaelidje@consultech.ci>
2018-12-15 06:08:35 +0000

    manage pipe for time

--- src/app/second-to-time.pipe.ts
+++ src/app/second-to-time.pipe.ts
@@ -10,11 +10,13 @@ export class SecondToTimePipe implements PipeTransform {
     let hours = Math.floor(sec_num / 3600);
     let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     let seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-    if (hours < 10) {let hour = "0" + hours; }
-    if (minutes < 10) {let minute = "0" + minutes; }
-    if (seconds < 10) {let second = "0" + seconds; }
-    return hours + 'h' + minutes + 'm' + seconds + '.' + parseInt(JSON.stringify(value).split('.')[1].substring(0, 3));
+    let hour = JSON.stringify(hours)
+    let minute = JSON.stringify(minutes)
+    let second = JSON.stringify(seconds)
+    if (hours < 10) { hour = "0" + hours; }
+    if (minutes < 10) { minute = "0" + minutes; }
+    if (seconds < 10) { second = "0" + seconds; }
+    return hour + ':' + minute + ':' + second + '.' + parseInt(JSON.stringify(value).split('.')[1].substring(0, 3));
     //return seconds;
   }
 
