4a85856d39120f49b516a5ed7d798e0e3033fc95
Nougbele Daniel Jonatan <001ndj@gmail.com>
2020-12-19 14:08:05 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://63.33.110.240:3000',
+  apiUrl: 'https://rallye-api.herokuapp.com',
   apiCountries: 'https://restcountries.eu/rest/v2'
 };
