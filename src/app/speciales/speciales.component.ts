d5a1629f21aa784032bd2f492e8bb621ab86ead3
agatapouglof <001ndj@gmail.com>
2021-01-20 11:51:36 +0100

    ssv for Aboisso rallye 2021

--- src/app/speciales/speciales.component.ts
+++ src/app/speciales/speciales.component.ts
@@ -14,12 +14,12 @@ export class SpecialesComponent implements OnInit {
   ramdom:number;
   tab_images = [
     
-    "http://images.larepubliquedespyrenees.fr/2017/03/01/58b71395a43f5ea4166d895e/golden/le-35e-rallye-d-arzacq-c-est-ce-week-end.jpg",
-    "https://cdn-s-www.ledauphine.com/images/837830C2-C1BD-49B5-80E0-47275C792976/LDL_V0_12/sebastien-ogier-a-remporte-le-rallye-de-grande-bretagne-photo-geoff-caddick-afp-1539653919.jpg",
-    "http://sf2.sport365.fr/wp-content/uploads/se/2016/01/299112-750x410.jpg",
-    "http://pilote-de-course.com/wp-content/uploads/2017/01/Dakar-2017-Etape-12-3008-DKR-Peterhansel-678x381.jpg",
-    "http://4x4terredusud.com/wp-content/uploads/rallye-maroc-2017.jpg",
-    "https://media.ouest-france.fr/v1/pictures/ecf708afdef121d3cb7bcc6aabc79611-rallye-wrc-canal-acquiert-les-droits-de-diffusion-partir-de-2019.jpg?width=1260&height=712&fill=0&focuspoint=56%2C65&cropresize=1"
+    "/assets/images/SSV/ssv1.jpeg",
+    "/assets/images/SSV/ssv2.jpeg",
+    "/assets/images/SSV/ssv4.jpeg",
+    "/assets/images/SSV/ssv5.jpeg",
+    "/assets/images/SSV/ssv6.jpeg",
+    "/assets/images/SSV/ssv7.jpeg",
   ]
   ngOnInit() {
     this.getAllSpeciales()
